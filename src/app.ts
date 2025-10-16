2fba3568999e00b513541cbc7e92ab3b19ed4c7b
PaulAjii <paulajii134@gmail.com>
2025-10-16 15:09:06 +0100

    feat: used the cat fact fetch service in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 
+import { fetchCatFact } from "./cat_service";
+
 const app = express();
 
 app.use(cors());
@@ -10,11 +12,29 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = 3000;
 
 // Route
-app.get("/", (_req: Request, res: Response) => {
-  res.status(200).json({
-    status: "success",
-    message: "Welcome to HNGi13 Stage Zero",
-  });
+app.get("/me", async (_req: Request, res: Response) => {
+  try {
+    const catFact = await fetchCatFact();
+
+    res.status(200).json({
+      status: "success",
+      user: {
+        email: "peepeeajii@gmail.com",
+        name: "Paul Ajijola",
+        stack: "Node.js/Express",
+      },
+      timestamp: new Date().toISOString(),
+      fact: catFact,
+    });
+  } catch (error: any) {
+    const statusCode = error.message.includes("timeout") ? 504 : 500;
+    const errorResponse = {
+      status: "error",
+      message: error.message || "Internal Server Error",
+      timestamp: new Date().toISOString(),
+    };
+    res.status(statusCode).json(errorResponse);
+  }
 });
 
 app.listen(PORT, () => {
